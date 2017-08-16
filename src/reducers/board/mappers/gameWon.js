902405cb897ac72bee143f1660ded22710bec585
David Banks <davidgbanks@gmail.com>
2017-08-16 20:41:56 +1200

    clear all uncleared cells when game is won

--- src/reducers/board/mappers/gameWon.js
+++ src/reducers/board/mappers/gameWon.js
@@ -1,6 +1,17 @@
+import {CELL_STATE_UNCLEARED} from '../../../constants';
+import {mapProximityCountToCellState} from '../../../utils/mapProximityCountToCellState';
+
 export const gameWonMapper = (state, action) => {
+	// clear any remaining uncleared cells
+	const {proximity, board} = state;
+	const boardFinal = [...board]
+		.map((cellState, i) => cellState === CELL_STATE_UNCLEARED ?
+			mapProximityCountToCellState(proximity[i]) :
+			cellState
+		);
 	return {
 		...state,
+		board: boardFinal,
 		isFinished: true,
 		lastGameLost: false
 	};
