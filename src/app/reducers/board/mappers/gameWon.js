9ce8013c377a20a961ef340f2f35a7bced2c0510
David Banks <davidgbanks@gmail.com>
2017-08-18 20:29:23 +1200

    increase cell size slightly, fix bug with unmarked mines when game won

--- src/app/reducers/board/mappers/gameWon.js
+++ src/app/reducers/board/mappers/gameWon.js
@@ -1,4 +1,6 @@
-import {CELL_STATE_FLAGGED, CELL_STATE_REVEAL_MINE_FOUND, CELL_STATE_UNCLEARED} from '../../../constants';
+import {
+	CELL_STATE_FLAGGED, CELL_STATE_REVEAL_MINE_FOUND, CELL_STATE_UNCLEARED, PROXIMITY_IS_MINE
+} from '../../../constants';
 import {mapProximityCountToCellState} from '../../../utils/mapProximityCountToCellState';
 
 export const gameWonMapper = (state, action) => {
@@ -6,9 +8,10 @@ export const gameWonMapper = (state, action) => {
 	const {proximity, board} = state;
 	const boardFinal = board
 		.map((cellState, i) => {
+			const isMine = proximity[i] === PROXIMITY_IS_MINE;
 			switch (cellState) {
 				case CELL_STATE_UNCLEARED:
-					return mapProximityCountToCellState(proximity[i]);
+					return isMine ? CELL_STATE_REVEAL_MINE_FOUND : mapProximityCountToCellState(proximity[i]);
 				case CELL_STATE_FLAGGED:
 					return CELL_STATE_REVEAL_MINE_FOUND;
 				default:
