67604fd82d14cbe1a574ac950cb0f1432f2a4032
David Banks <davidgbanks@gmail.com>
2017-08-19 18:13:36 +1200

    save vendors css as dependencies instead of loading from cdn

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Provider} from 'react-redux';
 import Game from './Game';
 import store from '../store';
 
-class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<div className="app">
-					<Game/>
-				</div>
-			</Provider>
-		);
-	}
-}
+const App = () => (
+	<Provider store={store}>
+		<div className="app">
+			<Game/>
+		</div>
+	</Provider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
+
+import './assets/vendors.css';
 import './assets/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
