112bdbce93698fc864a4f0c87beb9e93e637045d
David Banks <davidgbanks@gmail.com>
2017-08-20 09:28:35 +1200

    switch to SCSS p1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
-import './assets/styles.css';
+import './assets/styles/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
