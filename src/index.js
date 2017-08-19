8b809d795c73013ef0ad11871a5c6547ba92ee65
David Banks <davidgbanks@gmail.com>
2017-08-19 18:22:22 +1200

    revert split vendors css, react-scripts isn't supporting code splitting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
-
-import './assets/vendors.css';
 import './assets/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
